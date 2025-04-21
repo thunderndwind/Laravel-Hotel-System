ec84150ec6fa75be238a83c04818ee95296aa016
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2025-04-21 15:25:42 +0200

    feat: Adding Lravel-Countries Lib

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -41,15 +41,28 @@ class ClientController extends Controller
                 ->paginate(10);
         }
 
-        return view('clients.index', compact('clients'));
+        // return view('clients.index', compact('clients'));
+        return Inertia::render('Clients/Index', [
+            'clients' => $clients,
+            'can' => [
+                'create' => $user->can('create', Client::class),
+                'approve' => $user->can('approve', Client::class),
+            ]
+        ]);
     }
 
 
     // ============== Show the form for creating a new client (registration form) ==============
     public function create()
     {
-        $countries = \Countries::all()->pluck('name', 'iso_3166_2');
-        return view('clients.create', compact('countries'));
+        // $countries = \Countries::all()->pluck('name', 'iso_3166_2');
+        // return view('clients.create', compact('countries'));
+        $this->authorize('create', Client::class);
+
+        return Inertia::render('Clients/Create', [
+            'countries' => \Countries::all()->pluck('name', 'iso_3166_2'),
+            'genders' => ['male', 'female']
+        ]);
     }
 
     //    * Store a newly created client in storage.
@@ -108,8 +121,16 @@ class ClientController extends Controller
     {
         $this->authorize('view', $client);
 
-        $client->load(['user', 'approver', 'reservations.room.floor']);
-        return view('clients.show', compact('client'));
+        // $client->load(['user', 'approver', 'reservations.room.floor']);
+        // return view('clients.show', compact('client'));
+
+        return Inertia::render('Clients/Show', [
+            'client' => $client->load(['user', 'approver', 'reservations.room.floor']),
+            'can' => [
+                'update' => Auth::user()->can('update', $client),
+                'delete' => Auth::user()->can('delete', $client),
+            ]
+        ]);
     }
 
     // ==============Show the form for editing the specified client==============
@@ -117,8 +138,13 @@ class ClientController extends Controller
     {
         $this->authorize('update', $client);
 
-        $countries = \Countries::all()->pluck('name', 'iso_3166_2');
-        return view('clients.edit', compact('client', 'countries'));
+        // $countries = \Countries::all()->pluck('name', 'iso_3166_2');
+        // return view('clients.edit', compact('client', 'countries'));
+        return Inertia::render('Clients/Edit', [
+            'client' => $client->load('user'),
+            'countries' => \Countries::all()->pluck('name', 'iso_3166_2'),
+            'genders' => ['male', 'female']
+        ]);
     }
 
 
@@ -156,11 +182,18 @@ class ClientController extends Controller
     {
         $this->authorize('viewReservations', $client);
 
-        $reservations = $client->reservations()
-            ->with('room.floor')
-            ->latest()
-            ->paginate(10);
+        // $reservations = $client->reservations()
+        //     ->with('room.floor')
+        //     ->latest()
+        //     ->paginate(10);
 
-        return view('clients.reservations', compact('client', 'reservations'));
+        // return view('clients.reservations', compact('client', 'reservations'));
+        return Inertia::render('Clients/Reservations', [
+            'client' => $client,
+            'reservations' => $client->reservations()
+                ->with('room.floor')
+                ->latest()
+                ->paginate(10)
+        ]);
     }
 }
