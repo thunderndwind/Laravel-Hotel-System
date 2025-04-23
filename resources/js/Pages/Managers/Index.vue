8fb3fa0d6dda1fe22a80e470589be92502855f1e
NouranMohamedFouad <nouranloly@gmail.com>
2025-04-23 22:42:56 +0200

    feat: add shadcn

--- resources/js/Pages/Managers/Index.vue
+++ resources/js/Pages/Managers/Index.vue
@@ -1,22 +1,95 @@
-<script setup>
-import { Link, useForm } from '@inertiajs/vue3';
+<script setup lang="ts">
+import { h, defineProps } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table'
+import { Link } from '@inertiajs/vue3'
+import ManagersTable from '@/Pages/Managers/ManagersTable.vue'
+import { useForm } from '@inertiajs/vue3'
 
-defineProps({ managers: Object });
+defineProps({ managers: Object })
 
-const form = useForm({});
-function destroy(id) {
+const form = useForm({})
+function destroy(id: number) {
   if (confirm('Are you sure?')) {
-    form.delete(route('managers.destroy', id));
+    form.delete(route('managers.destroy', id))
   }
 }
+
+// Define table columns
+const columns: ColumnDef<any, any>[] = [
+  {
+    header: 'Avatar',
+    cell: ({ row }) =>
+      h('img', {
+        src: row.original.avatar_image,
+        class: 'w-12 h-12 rounded-full',
+      }),
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email',
+  },
+  {
+    accessorKey: 'national_id',
+    header: 'National ID',
+  },
+  {
+    accessorKey: 'phone_number',
+    header: 'Phone',
+  },
+  {
+    header: 'Actions',
+    cell: ({ row }) =>
+      h('div', { class: 'space-x-2' }, [
+        h(
+          Link,
+          {
+            href: route('managers.edit', row.original.id),
+            class: 'text-green-600',
+          },
+          'Edit'
+        ),
+        h(
+          Link,
+          {
+            href: route('managers.show', row.original.id),
+            class: 'text-blue-600',
+          },
+          'Show'
+        ),
+        h(
+          'button',
+          {
+            class: 'text-red-600',
+            onClick: () => destroy(row.original.id),
+          },
+          'Delete'
+        ),
+      ]),
+  },
+]
 </script>
 
 <template>
+  
   <div>
+
+
     <h1 class="text-xl font-bold mb-4">Managers List</h1>
+
+
+
     <Link :href="route('managers.create')" class="text-blue-500">Create New Manager </Link>
 
-    <table class="mt-4 w-full">
+
+    <div class="mt-4">
+      <ManagersTable :columns="columns" :data="managers.data" />
+    </div>
+
+    <!-- <table class="mt-4 w-full">
       <thead>
         <tr>
           <th>Avatar</th>
@@ -50,6 +123,6 @@ function destroy(id) {
           </td>
         </tr>
       </tbody>
-    </table>
+    </table> -->
   </div>
 </template>
